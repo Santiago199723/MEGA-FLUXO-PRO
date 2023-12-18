f170412b59e00c0fac51e891e668fb504d19fb3e
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2023-12-18 11:15:50 -0300

    MEGA FLUXO PRO

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,45 @@
-if (localStorage.getItem("token") == null) {
-  alert("Você precisa estar logado para acessar ao indicador! Suporte: https://t.me/+By1fd0M7ZCAwNmFh");
-  window.location.href = "./assets/html/signin.html";
-}
+// Import and configure Firebase
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-const logadoElement = document.querySelector("#logado");
-const logado = document.querySelector("#logado");
+const firebaseConfig = {
+  // Your Firebase config here
+};
 
-if (userLogado && userLogado.nome) {
-  logado.innerHTML = `<span style="font-family: 'Verdana', sans-serif; color: #4CAF50; font-size: 0.4em;">Aqui o sucesso é garantido.${userLogado.nome}</span>`;
-} else {
-  logado.innerHTML = `<span style="font-family: 'Verdana', sans-serif; color: #4CAF50; font-size: 0.4em;">Aqui o sucesso é garantido.</span>`;
-}
+firebase.initializeApp(firebaseConfig);
 
+// Export the auth object
+export const auth = firebase.auth();
 
+// Check if the user is authenticated
+auth.onAuthStateChanged((user) => {
+  if (!user) {
+    // User is not logged in
+    alert("Você precisa estar logado para acessar ao indicador! Suporte: https://t.me/+By1fd0M7ZCAwNmFh");
+    window.location.href = "./assets/html/signin.html";
+  } else {
+    // User is logged in
+    const userLogado = {
+      nome: user.displayName,
+      // Add other user properties as needed
+    };
 
+    const logadoElement = document.querySelector("#logado");
+    const logado = document.querySelector("#logado");
 
+    if (userLogado && userLogado.nome) {
+      logado.innerHTML = `<span style="font-family: 'Verdana', sans-serif; color: #4CAF50; font-size: 0.4em;">Aqui o sucesso é garantido.${userLogado.nome}</span>`;
+    } else {
+      logado.innerHTML = `<span style="font-family: 'Verdana', sans-serif; color: #4CAF50; font-size: 0.4em;">Aqui o sucesso é garantido.</span>`;
+    }
+  }
+});
 
+// Function to sign out
 function sair() {
-  localStorage.removeItem("token");
-  localStorage.removeItem("userLogado");
-  window.location.href = "./assets/html/signin.html";
+  auth.signOut().then(() => {
+    window.location.href = "./assets/html/signin.html";
+  }).catch((error) => {
+    console.error("Error signing out:", error);
+  });
 }
