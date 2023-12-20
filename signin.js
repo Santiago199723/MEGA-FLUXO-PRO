0c471cf4f211f305f4a00392fabc1f35a714010e
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2023-12-20 00:34:10 -0300

    MEGA FLUXO PRÓ

--- signin.js
+++ signin.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btnEye = document.querySelector('.fa-eye');
-  const inputSenha = document.querySelector('#senha');
-  const msgError = document.querySelector('#msgError');
-  const usuario = document.querySelector('#usuario');
-  const senha = document.querySelector('#senha');
+  let btn = document.querySelector('.fa-eye');
+  let inputSenha = document.querySelector('#senha');
+  let msgError = document.querySelector('#msgError');
+
+  btn.addEventListener('click', () => {
+    if (inputSenha.getAttribute('type') === 'password') {
+      inputSenha.setAttribute('type', 'text');
+    } else {
+      inputSenha.setAttribute('type', 'password');
+    }
+  });
 
   function showErrorMessage(message) {
     msgError.style.display = 'block';
@@ -16,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function isValidEmail(email) {
-    // Expressão regular para validar o formato do e-mail
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
@@ -26,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
     hideErrorMessage();
     showLoading();
 
-    // Verificar se ambos os campos estão preenchidos
     if (!usuario.value || !senha.value) {
       showErrorMessage('Preencha todos os campos para poder logar.');
       hideLoading();
@@ -36,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const email = usuario.value;
     const password = senha.value;
 
-    // Validar o formato do e-mail
     if (!isValidEmail(email)) {
       showErrorMessage('Formato de e-mail inválido.');
       hideLoading();
@@ -73,48 +76,6 @@ document.addEventListener('DOMContentLoaded', function () {
     window.location.href = "pages/register/register.html";
   }
 
-  function toggleEmailErrors() {
-    const email = usuario.value;
-    form.emailRequiredError().style.display = email ? "none" : "block";
-    form.emailInvalidError().style.display = isValidEmail(email) ? "none" : "block";
-  }
-
-  function togglePasswordErrors() {
-    const password = senha.value;
-    form.passwordRequiredError().style.display = password ? "none" : "block";
-  }
-
-  function toggleButtonsDisable() {
-    const emailValid = isEmailValid();
-    form.recoverPasswordButton().disabled = !emailValid;
-
-    const passwordValid = isPasswordValid();
-    form.loginButton().disabled = !emailValid || !passwordValid;
-  }
-
-  function isEmailValid() {
-    const email = usuario.value;
-    if (!email) {
-      return false;
-    }
-    return isValidEmail(email);
-  }
-
-  function isPasswordValid() {
-    return senha.value ? true : false;
-  }
-
-  const form = {
-    email: () => document.getElementById("email"),
-    emailInvalidError: () => document.getElementById("email-invalid-error"),
-    emailRequiredError: () => document.getElementById("email-required-error"),
-    loginButton: () => document.getElementById("login-button"),
-    password: () => document.getElementById("password"),
-    passwordRequiredError: () => document.getElementById("password-required-error"),
-    recoverPasswordButton: () => document.getElementById("recover-password-button"),
-  };
-
-  // Evento de clique no botão "Entrar"
   const btnEntrar = document.querySelector('#btnEntrar');
   if (btnEntrar) {
     btnEntrar.addEventListener('click', entrar);
